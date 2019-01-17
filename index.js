a227578c182a575c351ed3a043219b5028ef129f
Joan in DC <joanrigdon@gmail.com>
2019-01-17 19:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ document.addEventListener('keydown', function(e) {
 })
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
+  var rightNumbers = dodger.style.left.replace('px', '')
   var right = parseInt(rightNumbers, 10)
  
-  if (right < 360) {
-    dodger.style.right = `${right - 1}px`
+  if (left < 360) {
+    dodger.style.left = `${right - 1}px`
   }
 }
 

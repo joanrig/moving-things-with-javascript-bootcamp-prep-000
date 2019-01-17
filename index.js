5fbc4e6b628564c1ac2ed6f649aeb7cb2ce5bbef
Joan in DC <joanrigdon@gmail.com>
2019-01-17 19:50:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function moveDodgerLeft() {
   }
 }
 
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right < 360) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
